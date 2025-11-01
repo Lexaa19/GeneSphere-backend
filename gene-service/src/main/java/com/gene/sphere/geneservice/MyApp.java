418d902d4b6d457e7a1ff80a52ca8d6aa3d888e9
Lexaa19 <caliopi.durdunescu@yahoo.com>
2025-11-01 13:15:14 +0200

    Enable caching

--- gene-service/src/main/java/com/gene/sphere/geneservice/MyApp.java
+++ gene-service/src/main/java/com/gene/sphere/geneservice/MyApp.java
@@ -2,9 +2,11 @@ package com.gene.sphere.geneservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableCaching
 public class MyApp {
     public static void main(String[] args) {
         SpringApplication.run(MyApp.class, args);
