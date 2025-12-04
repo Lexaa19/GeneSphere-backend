85afeef12ce6b246fed5fa159ab487dd4b6ff09b
Lexaa19 <caliopi.durdunescu@yahoo.com>
2025-12-04 15:21:10 +0200

    Update entry point

--- gene-service/src/main/java/com/gene/sphere/geneservice/MyApp.java
+++ gene-service/src/main/java/com/gene/sphere/geneservice/MyApp.java
@@ -2,11 +2,15 @@ package com.gene.sphere.geneservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableCaching
+@EnableJpaRepositories(basePackages = "com.gene.sphere.geneservice.repository")
+@EntityScan(basePackages = "com.gene.sphere.geneservice.model")
 public class MyApp {
     public static void main(String[] args) {
         SpringApplication.run(MyApp.class, args);
